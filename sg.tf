e6a06445c625fb2e83088e8272b0614d9834b6b8
unknown <Pratima Tiwari@earth>
2025-07-04 18:12:05 +0530

    updated buildspec

--- sg.tf
+++ sg.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "studentApp_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "studentApp_sg_ipv4" {
   security_group_id = aws_security_group.studentApp_sg.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = "10.0.0.0/8"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
