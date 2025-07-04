d42831b899c6ee8fd4522877cc84a5539d6aed4a
unknown <Pratima Tiwari@earth>
2025-07-04 18:22:37 +0530

    updated buildspec

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ terraform {
   }
 }
 
-resource "aws_instance" "student_host1" {
+resource "aws_instance" "studentApp_host1" {
     ami = "ami-05ffe3c48a9991133"
     instance_type = "t2.micro"
     security_groups = [aws_security_group.studentApp_sg.id]
